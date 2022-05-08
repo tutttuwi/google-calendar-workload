3436cc0c8b405e8f59cf632c8c307e9daf091916
t-tsutsui <t.tsutsui9117@gmail.com>
2022-05-09 00:30:30 +0900

    create sample dom

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -10,25 +10,38 @@ window.onload = function () {
    * スクリプト処理
    */
   function insertUI() {
-    let divEl = document.createElement("div");
-    let pEl = document.createElement("p");
-    pEl.innerText = "テストChrome拡張機能";
-    divEl.appendChild(pEl);
-    let selectHtmlStr = `<select id='select-form' placeholder='選んでね！'>
-    <option disabled selected placeholder>選択してね！</option>
-    <option value="1">アップル</option>
-    <option value="2">オレンジ</option>
-    <option value="3">バナナ</option>
-    <option value="4">メロン</option>
-  </select>`;
+    let htmlStr = `<div id="app-workload"><div style="display: flex;width: calc(578px + 56px + (100% - 1049px)*0.16666667);padding: 8px 0;flex: 1 1 auto;margin-left: 60px;align-items: center;justify-content: start;">
+  <select id="select-project" placeholder="選んでね！" style="flex-grow: 1;margin: 0px 5px;">
+    <option disabled="" selected="" placeholder="">プロジェクト選択</option>
+    <option value="1">〇〇案件</option>
+    <option value="2">☓☓対応</option>
+    <option value="3">▲▲改善</option>
+  </select>
+  <select id="select-phase" placeholder="選んでね！" style="flex-grow: 1;margin: 0px 5px;">
+    <option disabled="" selected="" placeholder="">フェーズ選択</option>
+    <option value="1">要件定義</option>
+    <option value="2">調査</option>
+    <option value="3">基本設計</option>
+    <option value="4">製造</option>
+  </select>
+  <select id="select-task" placeholder="選んでね！" style="flex-grow: 1;margin: 0px 5px;">
+    <option disabled="" selected="" placeholder="">タスク選択</option>
+    <option value="1">○○機能追加</option>
+    <option value="2">☓☓修正</option>
+    <option value="3">▲▲のスケジュール整理</option>
+    <option value="4">リファクタリング</option>
+  </select></div></div>`;
     const parser = new DOMParser();
-    const selectHtmlDom = parser.parseFromString(selectHtmlStr, "text/html");
-    divEl.appendChild(selectHtmlDom.getRootNode().body);
-    if (document.querySelector("[aria-label='タイトル']")) {
+    const selectHtmlDom = parser.parseFromString(htmlStr, "text/html");
+    const domEl = selectHtmlDom.body.childNodes[0];
+    if (
+      document.querySelector("[aria-label='タイトル']") &&
+      !document.querySelector("#app-workload")
+    ) {
       document
         .querySelector("[aria-label='タイトル']")
         .parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.after(
-          divEl
+          domEl
         );
     }
   }
